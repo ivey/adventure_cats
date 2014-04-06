8ee49dd87d42eb5abec62d2f337fe8c59b98a8ad
Michael Ivey <michael.ivey@riotgames.com>
2014-04-06 09:19:59 -0700

    working physics l1

--- level1.js
+++ level1.js
@@ -1,35 +1,42 @@
 var level_1 = {
-  create: function() {
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.game.stage.backgroundColor = '#00bfff';
-    this.cursors = this.game.input.keyboard.createCursorKeys();
+  platforms:null,
+  player:null,
 
-    platforms = this.game.add.group();
+  create: function() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.stage.backgroundColor = '#00bfff';
+    cursors = game.input.keyboard.createCursorKeys();
+
+    platforms = game.add.group();
     platforms.enableBody = true;
 
     var ground = platforms.create(0, this.game.world.height - 62, 'dirt');
     ground.scale.setTo(2, 2);
     ground.body.immovable = true;
 
-    var ledge = platforms.create(400, 400, 'dirt');
+    var ledge = platforms.create(400, 350, 'dirt');
     ledge.body.immovable = true;
-    ledge = platforms.create(-150, 250, 'dirt');
+    ledge = platforms.create(-150, 170, 'dirt');
     ledge.body.immovable = true;
 
-    player = this.game.add.sprite(32, this.game.world.height - 190, 'cat1');
-    this.game.physics.arcade.enable(player);
+    player = game.add.sprite(32, game.world.height - 190, 'cat1');
+    game.physics.arcade.enable(player);
 
     player.body.bounce.y = 0.2;
     player.body.gravity.y = 300;
     player.body.collideWorldBounds = true;
+  },
+
+  update: function() {
+    game.physics.arcade.collide(player, platforms);
 
     player.body.velocity.x = 0;
 
-    if (this.cursors.left.isDown)
+    if (cursors.left.isDown)
     {
       player.body.velocity.x = -150;
     }
-    else if (this.cursors.right.isDown)
+    else if (cursors.right.isDown)
     {
       player.body.velocity.x = 150;
     }
@@ -38,13 +45,9 @@ var level_1 = {
       // player.animations.stop();
     }
 
-    if (this.cursors.up.isDown && player.body.touching.down)
+    if (cursors.up.isDown && player.body.touching.down)
     {
       player.body.velocity.y = -350;
     }
-  },
-
-  update: function() {
-    this.game.physics.arcade.collide(player, platforms);
   }
 };
